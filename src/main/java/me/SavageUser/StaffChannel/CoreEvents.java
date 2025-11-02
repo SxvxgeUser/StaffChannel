236326427ff2ff4c65ac4089267fb05f981c8544
SavageUser <LimitlessPowerHD@gmail.com>
2025-11-02 18:04:19 -0500

    Update CoreEvents.java

--- src/main/java/me/SavageUser/StaffChannel/CoreEvents.java
+++ src/main/java/me/SavageUser/StaffChannel/CoreEvents.java
@@ -1,6 +1,5 @@
 package me.SavageUser.StaffChannel;
 
-import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -8,10 +7,10 @@ import org.bukkit.event.player.PlayerJoinEvent;
 
 public class CoreEvents implements Listener {
 
-    private Core core;
+    private Core plugin;
 
-    public CoreEvents(Core core) {
-    this.core = core;
+    public CoreEvents(Core plugin) {
+        this.plugin = plugin;
 
     }
 
@@ -19,19 +18,16 @@ public class CoreEvents implements Listener {
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
 
-        if (player.isOp() || core.hasAdminPerm(player)|| core.hasStaffPerm(player)) {
+        if (player.isOp() || plugin.hasAdminPerm(player)|| plugin.hasStaffPerm(player)) {
             if (Core.StaffConfig.getConfigOption("Player-Settings." + player.getName()) == null) {
-                core.StaffConfig.createPlayerSettings(player.getName());
+                plugin.StaffConfig.createPlayerSettings(player.getName());
             }
 
-            if (!core.hasStaffChatEnabled(player))  {
-                player.sendMessage(ChatColor.translateAlternateColorCodes('&', core.StaffConfig.getString("TAG") + " " + "§7You currently have the staff chat §4disabled§7!"));
-                player.sendMessage(ChatColor.translateAlternateColorCodes('&', core.StaffConfig.getString("TAG") + " " + "§7Type §e/staffchat togglechat §7to §are-enable §7it!"));
+            if (!plugin.hasStaffChatEnabled(player))  {
+                player.sendMessage("§6SC§8: §7You currently have the admin/staff chat §4disabled§7!");
+                player.sendMessage("§6SC§8: §7Type §e/staffchat togglechat §7to §are-enable §7it!");
             }
         }
 
     }
-
-
-
 }
